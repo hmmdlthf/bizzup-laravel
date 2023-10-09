abeebcaa5d17aba10bc1157f2ff00e038224f0b8
kevin_jorn <Althaf>
2023-10-09 15:01:47 +0530

    moddify

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,6 @@ class DatabaseSeeder extends Seeder
         $this->call(StateSeeder::class);
         $this->call(CitySeeder::class);
         $this->call(SocialTypeSeeder::class);
+        $this->call(NfcCardSeeder::class);
     }
 }
